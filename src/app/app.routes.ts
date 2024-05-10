82846863c54767582eb15a3b686b16010a75c66a
unknown <lucasrpereira29@gmail.com>
2024-05-10 00:47:10 -0300

    sobre nos page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { EventoListaComponent } from './evento-lista/evento-lista.component';
 import { EventoFormComponent } from './evento-form/evento-form.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
     {path: 'listagem', component: EventoListaComponent},
     {path: 'cadastro', component: EventoFormComponent},
+    {path: 'sobre', component: SobreComponent},
     {path: '', redirectTo:'/listagem', pathMatch: 'full'},
 ];
